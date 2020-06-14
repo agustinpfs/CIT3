648600ccd243897228d85ddbe6212d91237495ae
Agustin <agu@cursosit.com>
2020-06-14 20:36:06 -0300

    agrego script para mostrar solo el 1º slide

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let slideNum = 1; 
+showSlides();
+
+function showSlides() { 
+    let slides = document.getElementsByClassName("Slide__js");    
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }        
+    slides[slideNum - 1].style.display = "block"; 
+}
