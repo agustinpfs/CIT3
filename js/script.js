20b43aeb40853f3ed484cc28d7d506d3920ec2fa
Agustin <agu@cursosit.com>
2020-06-14 21:26:10 -0300

    Script de Slider finalizado. Se agragaron condicionales para que no se corte.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 let slideNum = 1; 
-showSlides();
+showSlides(slideNum);
 
 function passSlides(n) {
-    slideNum += n
-    showSlides();
+    showSlides(slideNum += n);
 }
 
-function showSlides() { 
-    let slides = document.getElementsByClassName("Slide__js");    
+function showSlides(n) { 
+    let slides = document.getElementsByClassName("Slide__js");
+    if (n > slides.length) {slideNum = 1}    
+    if (n < 1) {slideNum = slides.length}
     for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }        
